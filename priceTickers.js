d5ce661126d5395ec891ad51b3bd5b0c87b81c12
Romeo <Romeofpablo@outlook.com>
2023-01-26 23:22:52 -0500

    Changed api location and alphabetical order buton

--- priceTickers.js
+++ priceTickers.js
@@ -6,26 +6,50 @@
 //     binanace.innerText = parseFloat(stockObject.p).toFixed(2) ;
 // }
 
+// let bnb = document.getElementById('bnb');
+// let revo = document.getElementById('revo');
+
+
+// var liveprice = {
+//     "async": true,
+//     "scroosDomain": true,
+//     "url": "https://api.coingecko.com/api/v3/simple/price?ids=binancecoin%2Crevomon&vs_currencies=usd",
+
+//     "method": "GET",
+//     "headers": {}
+// }
+
+// $.ajax(liveprice).done(function (response){
+//     bnb.innerHTML = response.binancecoin.usd;
+//     revo.innerHTML = response.revomon.usd;
+
+// });
+
+
 let bnb = document.getElementById('bnb');
 let revo = document.getElementById('revo');
 
-
-var liveprice = {
+function updatePrices() {
+  var liveprice = {
     "async": true,
     "scroosDomain": true,
     "url": "https://api.coingecko.com/api/v3/simple/price?ids=binancecoin%2Crevomon&vs_currencies=usd",
-
     "method": "GET",
     "headers": {}
-}
+  }
 
-$.ajax(liveprice).done(function (response){
+  $.ajax(liveprice).done(function (response) {
     bnb.innerHTML = response.binancecoin.usd;
     revo.innerHTML = response.revomon.usd;
+  });
+}
 
-});
+// Make the initial request
+updatePrices();
 
-  
+// Set the interval for subsequent requests
+setInterval(updatePrices, 20000);
+
 
 
 
